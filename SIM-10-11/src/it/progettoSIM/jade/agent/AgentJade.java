bf185dc668922590cf190ddf7a028ba365bf6c23
aenima.rm@gmail.com <aenima.rm@gmail.com@3a20fc0f-87df-faab-ae6e-b1454ec12d7f>
2011-06-15 16:46:24 +0000

    Aggiunta relazione e inizio creazione progetto SIM-10-11

---
+++ SIM-10-11/src/it/progettoSIM/jade/agent/AgentJade.java
@@ -0,0 +1,33 @@
+/* La classe che rappresenta gli agenti JADE
+ * 
+ */
+package it.progettoSIM.jade.agent;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+abstract public class AgentJade extends Agent {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//Campi statici relativi ai vari comandi
+	public final static String CMDREAD = "rd";
+	public final static String CMDIN = "in";
+	public final static String CMDREPLY = "reply";
+	public final static String CMDTS = "ts";
+	public final static String CMDOUT = "out";
+	public final static String CMDDEL = "del";
+	public final static String CMDFAIL = "fail";
+	public final static String CMDOK = "ok";
+	
+	public AgentJade(){
+	}
+	
+	// Funzione che formatta l'ACLMessage in maniera corretta
+	public static ACLMessage createCommandMessage(String cmd) {
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		msg.addUserDefinedParameter("command", cmd);
+		return msg;
+	}
+}
